9dd53713ae6371781bc5b7254847a72a14fa9d06
Gabriel Doon <doongs@dukes.jmu.edu>
2021-03-04 20:12:52 -0500

    Remove old

--- old/assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  //console.log(sessionStorage.getItem("visited"));
-  if (sessionStorage.getItem("visited") == null) {
-    $("#alert-modal").modal("show");
-    sessionStorage.setItem("visited", true);
-  }
-})();
